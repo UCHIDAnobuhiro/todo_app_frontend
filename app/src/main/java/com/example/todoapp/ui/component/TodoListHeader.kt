c1190ab8cb31d4686b898a6d709604305ba3c78c
UCHIDAnobuhiro <92204227+UCHIDAnobuhiro@users.noreply.github.com>
2025-08-05 08:14:52 +0900

    add/ヘッダーコンポーネントの作成

---
+++ app/src/main/java/com/example/todoapp/ui/component/TodoListHeader.kt
@@ -0,0 +1,14 @@
+package com.example.todoapp.ui.component
+
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun  TodoListHeader(onSettingsClick: () -> Unit) {
+    TopAppBar(
+        title = { Text("Todoリスト") },
+    )
+}
