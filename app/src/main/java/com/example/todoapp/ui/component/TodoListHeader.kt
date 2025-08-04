7cecc8bccee4e3da328d4b734f0e9bbab4eefe7a
UCHIDAnobuhiro <92204227+UCHIDAnobuhiro@users.noreply.github.com>
2025-08-05 08:17:06 +0900

    del/使用してない引数の削除

--- app/src/main/java/com/example/todoapp/ui/component/TodoListHeader.kt
+++ app/src/main/java/com/example/todoapp/ui/component/TodoListHeader.kt
@@ -7,7 +7,7 @@ import androidx.compose.runtime.Composable
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun  TodoListHeader(onSettingsClick: () -> Unit) {
+fun  TodoListHeader() {
     TopAppBar(
         title = { Text("Todoリスト") },
     )
