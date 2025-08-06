ac5ae57cb694a69dcbec960cc3a8d8a6888d7b6c
UCHIDAnobuhiro <92204227+UCHIDAnobuhiro@users.noreply.github.com>
2025-08-06 11:17:13 +0900

    del/不要なimportを削除

--- app/src/main/java/com/example/todoapp/viewmodel/TodoViewModel.kt
+++ app/src/main/java/com/example/todoapp/viewmodel/TodoViewModel.kt
@@ -1,6 +1,5 @@
 package com.example.todoapp.viewmodel
 
-import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.todoapp.data.model.Todo
